d19110ed47510dcd303e378f5ae4926242acd03a
robertkottelin <robert.kottelin@gmail.com>
2023-10-14 10:33:13 +0200

    cleanup

--- psi4-simulation.py
+++ psi4-simulation.py
@@ -2,43 +2,28 @@ import psi4
 import numpy as np
 from typing import Union, Tuple
 
-# Print the global options avaliable in Psi4
-print(psi4.core.print_global_options())
+# Constants
+INITIAL_ITERATIONS = 10
+INCREMENT_ITERATIONS = 10
+MAX_LOOPS = 20
 
-psi4.set_options(
-    {
-        "GEOM_MAXITER": 100,
-        "GUESS": "sad",
-        "REFERENCE": "rhf",
-        "SCF_TYPE": "direct",
-        "INTS_TOLERANCE": 1.0e-8
-    }
-)
+# Set Global Options for Psi4
+psi4.core.print_global_options()
+
+psi4.set_options({
+    "GEOM_MAXITER": 100,
+    "GUESS": "sad",
+    "REFERENCE": "rhf",
+    "SCF_TYPE": "direct",
+    "INTS_TOLERANCE": 1.0e-8
+    })
+
 
 psi4.set_memory("4GB")
-psi4.set_output_file("p4_output.txt", 
-                     append=False,
-                     loglevel=20, 
-                     print_header=True
-                    )
+psi4.set_output_file("p4_output.txt", append=False, loglevel=20, print_header=True)
 psi4.core.set_num_threads(4)
 
-# molecule = psi4.geometry(
-#     """
-#     0 1
-#     O        0.00000        0.64670       -0.01863
-#     H        0.76026        0.61622       -0.62453
-#     H       -0.76026        0.61622       -0.62453
-#     --
-#     0 1
-#     O        0.00000       -0.04191        2.64300
-#     H        0.00000        0.08820        1.66859
-#     H        0.00000        0.87457        2.95608
-#     units angstrom
-#     symmetry c1
-#     """ 
-# )
-
+# Define Molecule
 molecule = psi4.geometry(
     """
     0 1
@@ -60,118 +45,75 @@ molecule = psi4.geometry(
     H                1.651218319857755    -1.311106113036174    -3.107019301039250
     units angstrom
     no_reorient
-    symmetry c1
-    """ 
+    symmetry c1  
+    """
 )
 
-def psi4_optimize(initial_iterations: int = 2,
-                  increment_iteration: int = 2,
-                  max_loop_count: int = 20,
-                  molecule: Union[psi4.core.Molecule, None] = None,
-                  method: str = "hf/cc-pvdz"
-                 ) -> Tuple[float, psi4.core.Wavefunction, dict]:
-    """
-    General function to run Psi4 optimization and restart a max of max_loop_count times incrementing the number
-    of iterations by increment_iteration from initial_iterations each loop
-    :param initial_iterations: integer the number of initial iterations
-    :param increment_iteration: integer the number of iteration increment by each time it fails to converge
-    :param max_loop_count: integer the maximum number of times to increment for convergence incomplete
-    :param molecule: psi4.core.Molecule the molecule object or None if given will save last geometry of each unconverged loop
-    :method: string define the quantum chem optimization method eg hf/cc-pvdz
-    :return: Tuple[float, psi4.core.Wavefunction, list] the energy, the wavefunction object and the history of conformations
-    """
-    
-    unconv = True
 
-    psi4.set_options(
-        {
-            "MAXITER": initial_iterations,
-            "GEOM_MAXITER": initial_iterations
-        }
-    )
+def psi4_optimize(molecule: psi4.core.Molecule,
+                  initial_iterations: int = INITIAL_ITERATIONS,
+                  increment_iteration: int = INCREMENT_ITERATIONS,
+                  max_loop_count: int = MAX_LOOPS,
+                  method: str = "hf/cc-pvdz"
+                  ) -> Tuple[float, psi4.core.Wavefunction, dict]:
+    """
+    Function to run Psi4 optimization. If it doesn't converge, it restarts up to max_loop_count times.
+    Each restart increases the number of iterations by increment_iteration.
+    """
+
+    psi4.core.be_quiet()
     
     loop_count = 0
+    molecule_traj = []
+    psi4.set_options({"MAXITER": initial_iterations, "GEOM_MAXITER": initial_iterations})
     
-    if molecule is not None:
-        molecule_traj = []
-
-    while unconv is True:
-        
+    while True:
         try:
-            
-            print("\ncount {}\n".format(loop_count))
+            print(f"\nLoop Count: {loop_count}\n")
             opt_energy, wfn, traj = psi4.optimize(method, return_wfn=True, return_history=True)
-            print("The optimized energy for this configuration is {:.6f} Hartree".format(opt_energy))
-            unconv = False
+            break
             
         except psi4.driver.ConvergenceError as cerr:
+            print(f"WARNING - Geometry unconverged will try restarting. Error: {str(cerr)}")
             
-            print("WARNING - Geometry unconverged will try restarting")
-            if molecule is not None:
-                bohr_coor = molecule.geometry()
-                bohr_coor.scale(psi4.constants.bohr2angstroms)
-                molecule_traj.append(bohr_coor.to_array())
-            unconv_wfn = cerr.wfn
-            unconv_wfn.to_file(unconv_wfn.get_scratch_filename(180))
-            psi4.set_options(
-                {
-                    "GUESS": "read",
-                    "MAXITER": initial_iterations + increment_iteration,
-                    "GEOM_MAXITER": initial_iterations + increment_iteration
-                }
-            )
+            bohr_coor = molecule.geometry()
+            bohr_coor.scale(psi4.constants.bohr2angstroms)
+            molecule_traj.append(bohr_coor.to_array())
             
-            loop_count = loop_count + 1
-
-            if max_loop_count <= loop_count:
+            loop_count += 1
+            if loop_count >= max_loop_count:
                 print("WARNING - Unconverged in maximum number of loops")
                 raise cerr
-                
-    if molecule is not None:
-        traj = {"coordinates": tuple(np.array(molecule_traj))}
 
+            psi4.set_options({
+                "GUESS": "read",
+                "MAXITER": initial_iterations + loop_count * increment_iteration,
+                "GEOM_MAXITER": initial_iterations + loop_count * increment_iteration
+            })
+
+    traj = {"coordinates": tuple(np.array(molecule_traj))}
     return opt_energy, wfn, traj
 
-# energy = psi4.energy("hf/cc-pvdz")
-# print("The energy for this configuration is {:.6f} Hartree".format(energy))
-# >>> -76.025611
+def save_geometry_to_pdb(molecule: psi4.core.Molecule, filename: str):
+    """
+    Save the molecule's geometry to a PDB file.
+    """
+    xyz_data = psi4.core.Molecule.save_string_xyz_file(molecule)
+    
+    with open(filename, "w") as pdb_file:
+        for i, line in enumerate(xyz_data.split("\n")[2:]):
+            if line.strip():
+                atom, x, y, z = line.split()
+                pdb_file.write(f"HETATM{int(i)+1:5d}  {atom:3s} LIG A   1    {float(x):8.3f}{float(y):8.3f}{float(z):8.3f}  1.00  0.00           {atom:2s}\n")
 
-# opt_energy = psi4.optimize("hf/cc-pvdz")
-# print("The optimized energy for this configuration is {:.6f} Hartree".format(opt_energy))
-# >>> -76.027054
+energy, wfn, traj = psi4_optimize(molecule)
+print(f"Optimized energy: {energy:.6f}")
 
-interaction_energy = psi4.energy("hf/cc-pvdz", bsse_type="cp")
-print("Interaction Basis Set Superposition Error (BSSE) energy {:.6f} Hartree".format(interaction_energy))
-energy, wfn = psi4.frequency("hf/cc-pvdz", molecule=molecule, return_wfn=True)
-# >>> -0.004137
+print("Calculating frequency...")
+frequencyenergy, wfn = psi4.frequency("hf/cc-pvdz", molecule=molecule, return_wfn=True)
+print(f"Optimized final frequencyenergy: {frequencyenergy:.6f}")
 
-e_sapt0 = psi4.energy("sapt0/aug-cc-pVDZ")
-print("Interaction Symmetry Adapted Perturbation Theory (SAPT) energy {:.6f} Hartree".format(e_sapt0))
-# >>> -0.0067524
-
-
-# energy, wfn, traj = psi4_optimize(initial_iterations=10,
-#                                   increment_iteration=10,
-#                                   max_loop_count=20,
-#                                   method="hf/cc-pvdz",
-#                                  )
-
-# print("Optimized energy {:.6f}".format(energy))
-
-# oeprops = psi4.core.OEProp(wfn)
-# oeprops.add("DIPOLE")
-# oeprops.add("QUADRUPOLE")
-# oeprops.add("MULLIKEN_CHARGES")
-# oeprops.add("MULTIPOLE(4)")
-# oeprops.add("ESP_AT_NUCLEI")
-# oeprops.add("MO_EXTENTS")
-# oeprops.add("LOWDIN_CHARGES")
-# oeprops.add("WIBERG_LOWDIN_INDICES")
-# oeprops.add("MAYER_INDICES")
-# oeprops.add("NO_OCCUPATIONS")
-# oeprops.compute()
-
-# properties = wfn.variables()
-# print(properties["DIPOLE"])
+save_geometry_to_pdb(molecule, "optimized_geometry.pdb")
+print("Optimized geometry written to optimized_geometry.pdb")
 
 psi4.core.clean()
